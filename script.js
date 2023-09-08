3f5a75304320a78baee30483f3d2e15f76ea3a06
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-09-08 19:06:50 +0200

    Adding new functions for transactions

--- script.js
+++ script.js
@@ -43,6 +43,78 @@ const clearInputs = () => {
 	categorySelect.selectedIndex = 0;
 }
 
+const newTransaction = () => {
+	const transacion = document.createElement('div');
+	transacion.classList.add('transaction');
+	transacion.setAttribute('id', ID);
+	checkCategory(selectedCategory);
+
+	transacion.innerHTML = `
+	<p class="transaction-item">${nameInput.value} ${iconCategory}</p>
+	<p class="transaction-amount">${amountInput.value}PLN 
+	<button class="delete" onclick="deleteTransatcion(${ID})"><i class="fa-solid fa-trash-can"></i></button></p>
+	`;
+
+	amountInput.value > 0 ? earningsSection.appendChild(transacion) && transacion.classList.add('earnings') :
+		expensitureSection.appendChild(transacion) && transacion.classList.add('expenditure');
+
+
+	moneyArr.push(parseFloat(amountInput.value));
+	countMoney(moneyArr)
+	clousePanel();
+	ID++;
+	clearInputs();
+
+}
+
+const selectCategory = () => {
+	selectedCategory = categorySelect.options[categorySelect.selectedIndex].text;
+}
+
+const checkCategory = (transacion) => {
+	switch (transacion) {
+		case '(+) Earnings': iconCategory = '<i class="fa-solid fa-sack-dollar"></i>';
+			break;
+		case '(+) Bonus': iconCategory = '<i class="fa-regular fa-money-bill-1"></i>';
+			break;
+		case '(-) Shopping': iconCategory = '<i class="fa-solid fa-shirt"></i>';
+			break;
+		case '(-) Theater': iconCategory = '<i class="fa-solid fa-masks-theater"></i>';
+			break;
+		case '(-) Food': iconCategory = '<i class="fa-solid fa-utensils"></i>';
+			break;
+		case '(-) Cosmetics': iconCategory = '<i class="fa-solid fa-spa"></i>';
+			break;
+	}
+}
+
+const countMoney = almoney => {
+	const newMoney = almoney.reduce((x, y) => x + y);
+	availableMoney.textContent = `${newMoney} PLN`;
+}
+
+const deleteTransatcion = id => {
+	const transactionToDelete = document.getElementById(id);
+	const transactionAmount = parseFloat(transactionToDelete.childNodes[3].innerText);
+	const indexOfTransaction = moneyArr.indexOf(transactionAmount);
+
+
+	moneyArr.splice(indexOfTransaction, 1)
+
+	transactionToDelete.classList.contains('earnings') ? earningsSection.removeChild(transactionToDelete)
+		: expensitureSection.removeChild(transactionToDelete)
+
+	countMoney(moneyArr)
+}
+
+const deleteAllTransactions = () => {
+	earningsSection.innerHTML = '<h3>My earnings</h3>';
+	expensitureSection.innerHTML = '<h3>My expenditure</h3>';
+	availableMoney.innerHTML = '0 PLN'
+	moneyArr = [0];
+}
+
 addTransaction.addEventListener('click', secondPanel);
 cancelBtn.addEventListener('click', clousePanel);
 saveBtn.addEventListener('click', checkForm);
+deleteAllBtn.addEventListener('click', deleteAllTransactions);
