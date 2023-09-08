1bfaf2fb1f05aebff6c5c5fe687dc5d7d1db3ffa
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-09-08 18:47:21 +0200

    Adding secondPanel function, clousePanel function, checkForm function and adding eventListeners

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const expensitureSection = document.querySelector('.expenditure-area');
 const availableMoney = document.querySelector('.available-money');
 const transactionSection = document.querySelector('.add-transaction-panel');
 
-
 const nameInput = document.querySelector('#name');
 const amountInput = document.querySelector('#amount');
 const categorySelect = document.querySelector('#category');
@@ -19,4 +18,31 @@ let iconCategory;
 let selectedCategory;
 let moneyArr = [0];
 
+const secondPanel = () => {
+	transactionSection.style.display = 'flex';
+}
 
+const clousePanel = () => {
+	transactionSection.style.display = 'none';
+	clearInputs();
+}
+
+
+const checkForm = () => {
+
+	if (nameInput.value !== '' && amountInput.value !== '' && categorySelect.value !== 'none') {
+		newTransaction();
+	} else {
+		alert('error!');
+	}
+}
+
+const clearInputs = () => {
+	nameInput.value = '';
+	amountInput.value = '';
+	categorySelect.selectedIndex = 0;
+}
+
+addTransaction.addEventListener('click', secondPanel);
+cancelBtn.addEventListener('click', clousePanel);
+saveBtn.addEventListener('click', checkForm);
